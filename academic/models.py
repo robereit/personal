6a733123b6c94e8532aa511a3bf374db927d770a
robere <robere.it@gmail.com>
2014-12-26 18:25:20 -0500

    Updated models again. Here is a changelist: Paper: publication_type is now fixed to a set of choices. CourseInstance: role is now fixed to a set of choices
    Papers and teaching now populate with stored papers and course
    instances from the database.
    
    Views has several helper functions added, and the key view functions
    have been modified to show papers and teaching. In particular:
    
    format_authors: Given a paper, return a formatted string of authors
    	sorted by last name.
    
    get_item: A new registered filter for templates that allows you
    	to use dictionaries like normal (I don't know why this
    	isn't added by default)
    
    semester_compare: Given two strings of the form
    	"[Winter, Spring, Summer, Fall] [Year]"
    	sort them in descending lexicographic order but
    	keyed by YEAR FIRST.

--- academic/models.py
+++ academic/models.py
@@ -10,13 +10,25 @@ class Author(models.Model):
 class Paper(models.Model):
     author = models.ManyToManyField(Author)
     title = models.CharField(max_length=250)
-    publication_type = models.CharField(max_length=20)
+    
+    CONFERENCE = "CR"
+    JOURNAL = "JL"
+    MANUSCRIPT = "MA"
+    publication_type_choices = (
+        (CONFERENCE, "Conference"),
+        (JOURNAL, 'Journal'),
+        (MANUSCRIPT, 'Manuscript')
+    )
+    publication_type = models.CharField(max_length=20,
+                                        choices=publication_type_choices
+    )
+
     publication_venue = models.CharField(max_length=50)
     year = models.CharField(max_length=4)
     page_nums = models.CharField(max_length=25, blank=True)
     volume = models.SmallIntegerField(blank=True, null=True)
     number = models.SmallIntegerField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.title
 
@@ -27,11 +39,20 @@ class Course(models.Model):
     length = models.CharField(max_length=25)
 
     def __str__(self):
-        return self.course_code + " -- " + self.title
+        return self.course_code + ": " + self.title
 
 class CourseInstance(models.Model):
     course = models.ForeignKey(Course)
-    role = models.CharField(max_length=20)
+    
+    TEACHING_ASSISTANT = "TA"
+    COURSE_INSTRUCTOR = "CI"
+    role_choices = (
+        (TEACHING_ASSISTANT, "Teaching Assistant"),
+        (COURSE_INSTRUCTOR, "Course Instructor")
+    )
+    role = models.CharField(max_length=20,
+                            choices=role_choices
+    )
     semester = models.CharField(max_length=100, default="")
     
     def __str__(self):
--- academic/views.py
+++ academic/views.py
@@ -1,12 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.defaulttags import register
+
+from academic.models import Paper
+from academic.models import CourseInstance
 
 def index(request):
     context = {'headline' : "Main"}
     return render(request, 'academic/index.html', context)
 
-def papers(request):
-    return HttpResponse("This is the papers page.")
+def format_authors(paper):
+    """ Format the authors of the paper into a printable list. """
+    author_list = [p.name for p in paper.author.all()]
+    author_list = sorted(author_list, key=lambda x: x.split()[-1])
+    return ", ".join(author_list)
 
+def papers(request):
+    ## Format the author list of each paper
+    papers = Paper.objects.all()
+    papers = [x for x in reversed(sorted(papers, key=lambda x: x.year))]
+    papers = zip([format_authors(paper) for paper in papers], papers)
+    context = {'headline' : 'Projects and Publications', 'papers' : papers}
+    return render(request, 'academic/papers.html', context)
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+def semester_compare(cmp1, cmp2):
+    """ 
+    Given two strings of the form "[Winter, Summer, Fall] [Year]" order
+    them lexicographically (in the first coordinate) by Winter < Summer < Fall.
+    Years are ordered as normal.
+    """
+    split1 = cmp1.split()
+    split2 = cmp2.split()
+    order_map = {u"Winter" : 0, u"Spring" : 1, u"Summer" : 2, u"Fall" : 3}
+    if split1[1] < split2[1]:
+        return -1
+    elif split1[1] == split2[1]:
+        if order_map[split1[0]] < order_map[split2[0]]:
+            return -1
+        elif order_map[split1[0]] == order_map[split2[0]]:
+            return 0
+        else:
+            return 1
+    else:
+        return 1
+        
 def teaching(request):
-    return HttpResponse("This is the teaching page.")
+    all_courses = CourseInstance.objects.all()
+    ## plop all the courses into a semester map
+    course_semester_map = {}
+    for course in all_courses:
+        if course.semester in course_semester_map:
+            course_semester_map[course.semester].append(course)
+        else:
+            course_semester_map[course.semester] = [course]
+    all_semesters = reversed(sorted(course_semester_map.keys(), cmp=semester_compare))
+    context = {'headline' : "Teaching", 'course_semester_map' : course_semester_map, 'all_semesters' : all_semesters}
+    return render(request, 'academic/teaching.html', context)
