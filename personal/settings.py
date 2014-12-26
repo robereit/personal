e114b09155c655e64d7f1549e798a3b4f9d8d0d8
robere <robere.it@gmail.com>
2014-12-26 13:09:01 -0500

    - Moved base.html to a top-level directory

--- personal/settings.py
+++ personal/settings.py
@@ -24,6 +24,8 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR), 'templates']
+
 ALLOWED_HOSTS = []
 
 
@@ -82,3 +84,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
